487503e9706d3bbab652d07561aeae0232414a7a
unknown <madcat182@gmail.com>
2021-02-28 14:45:08 -0500

    General update

--- STAIR/Down Hold Explosion.lua
+++ STAIR/Down Hold Explosion.lua
@@ -1,10 +1,8 @@
 local t = Def.Sprite {
-	Texture="_Down Hold Explosion 3x1";
+	Texture="_Down Hold Explosion 2x1";
 	Frame0000=0;
-	Delay0000=0.01;
+	Delay0000=.033;
 	Frame0001=1;
-	Delay0001=0.01;
-	Frame0002=2;
-	Delay0002=0.01;
+	Delay0001=.033;
 };
 return t;
