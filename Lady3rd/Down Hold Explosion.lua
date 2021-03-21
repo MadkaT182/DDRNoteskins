6a57803770c2031c62ac14c010085fa6981fadab
unknown <madcat182@gmail.com>
2021-03-21 17:51:58 -0500

    Added more noteskins from 3rd mix

---
+++ Lady3rd/Down Hold Explosion.lua
@@ -0,0 +1,8 @@
+local t = Def.Sprite {
+	Texture="_Down Hold Explosion 2x1";
+	Frame0000=0;
+	Delay0000=.033;
+	Frame0001=1;
+	Delay0001=.033;
+};
+return t;
---
+++ Tracy3rd/Down Roll Head inactive.lua
@@ -0,0 +1,4 @@
+return Def.Sprite {
+	Texture=NOTESKIN:GetPath( '_down', 'rolli' );
+	Frames = Sprite.LinearFrames( 1, 1 );
+};
